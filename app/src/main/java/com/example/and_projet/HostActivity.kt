230287b76f037e7b3ca6afcd0ba9e05ce5b23909
Gaétan Zwick <gaetan.zwick@heig-vd.ch>
2022-06-01 12:52:32 +0200

    working-connexion

--- app/src/main/java/com/example/and_projet/HostActivity.kt
+++ app/src/main/java/com/example/and_projet/HostActivity.kt
@@ -1,10 +1,26 @@
 package com.example.and_projet
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.R
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.CallSuper
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.and_projet.databinding.ActivityHostBinding
-import com.example.and_projet.databinding.ActivityMainBinding
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.*
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import kotlin.text.Charsets.UTF_8
+
 
 class HostActivity : AppCompatActivity() {
 
@@ -25,11 +41,142 @@ class HostActivity : AppCompatActivity() {
         title = roomName
 
         binding.hostQuestionContent.text = question
+
+        startAdvertising()
     }
 
     companion object {
         const val ROOM_NAME_PARAMETER_KEY = "NAME_PARAMETER"
         const val ROOM_QUESTION_PARAMETER_KEY = "QUESTION_PARAMETER"
     }
+
+    fun startServer() {
+
+    }
+
+    private fun startAdvertising() {
+        val advertisingOptions = AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build()
+        Nearby.getConnectionsClient(applicationContext)
+            .startAdvertising(
+                roomName, "com.example.and_projet", connectionLifecycleCallback, advertisingOptions /* TODO CHANGE HOST TO "LOCALUSERNAME" */
+            )
+            .addOnSuccessListener(
+                OnSuccessListener { unused: Void? -> })
+            .addOnFailureListener(
+                OnFailureListener { e: Exception? -> })
+    }
+
+    private val connectionLifecycleCallback: ConnectionLifecycleCallback =
+        object : ConnectionLifecycleCallback() {
+            override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
+                AlertDialog.Builder(this@HostActivity)
+                    .setTitle("Accept connection to " + info.endpointName)
+                    .setMessage("Confirm the code matches on both devices: " + info.authenticationDigits)
+                    .setPositiveButton(
+                        "Accept"
+                    ) { dialog: DialogInterface?, which: Int ->  // The user confirmed, so we can accept the connection.
+                        Nearby.getConnectionsClient(applicationContext)
+                            .acceptConnection(endpointId, payloadCallback)
+                    }
+                    .setNegativeButton(
+                        R.string.cancel
+                    ) { dialog: DialogInterface?, which: Int ->  // The user canceled, so we should reject the connection.
+                        Nearby.getConnectionsClient(applicationContext).rejectConnection(endpointId)
+                    }
+                    .setIcon(R.drawable.ic_dialog_alert)
+                    .show()
+            }
+
+            override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+                when (result.status.statusCode) {
+                    ConnectionsStatusCodes.STATUS_OK -> {
+                        // We're connected! Can now start sending and receiving data.
+                        Log.i("DEBUG", "ConnectionsStatusCodes.STATUS_OK")
+                    }
+                    ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
+                        // The connection was rejected by one or both sides.
+                        Log.i("DEBUG", "ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED")
+                    }
+                    ConnectionsStatusCodes.STATUS_ERROR -> {
+                        // The connection broke before it was able to be accepted.
+                        Log.i("DEBUG", "ConnectionsStatusCodes.STATUS_ERROR")
+                    }
+                    else -> {
+                        // Unknown status code
+                        Log.i("DEBUG", "Unknown status code")
+                    }
+                }
+            }
+
+            override fun onDisconnected(endpointId: String) {
+                // We've been disconnected from this endpoint. No more data can be
+                // sent or received.
+            }
+
+        }
+
+    private val payloadCallback: PayloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            Log.i("DEBUG", String(payload.asBytes()!!, Charsets.UTF_8))
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+            // after the call to onPayloadReceived().
+            Log.i("DEBUG", endpointId)
+        }
+    }
+
+    // PERMISSIONS
+    private val REQUIRED_PERMISSIONS: Array<String> = arrayOf<String>(
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.BLUETOOTH_ADMIN,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.CHANGE_WIFI_STATE,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    private val REQUEST_CODE_REQUIRED_PERMISSIONS = 1
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onStart() {
+        super.onStart()
+        if (!hasPermissions(applicationContext, *REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+        }
+    }
+
+    /** Returns true if the app was granted all the permissions. Otherwise, returns false.  */
+    private fun hasPermissions(context: Context, vararg permissions: String): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    /** Handles user acceptance (or denial) of our permission request.  */
+    @CallSuper
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return
+        }
+        for (grantResult in grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(applicationContext, "MISSING PERMISSION", Toast.LENGTH_LONG).show()
+//                finish()
+                return
+            }
+        }
+        recreate()
+    }
+
 }
 
--- app/src/main/java/com/example/and_projet/ui/dashboard/JoinFragment.kt
+++ app/src/main/java/com/example/and_projet/ui/dashboard/JoinFragment.kt
@@ -1,14 +1,30 @@
 package com.example.and_projet.ui.dashboard
 
+import android.Manifest
+import android.R
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.CallSuper
+import androidx.core.app.ActivityCompat.recreate
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.and_projet.databinding.FragmentJoinBinding
 import com.example.and_projet.utils.ListAdapter
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.*
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+
 
 class JoinFragment : Fragment() {
 
@@ -39,6 +55,8 @@ class JoinFragment : Fragment() {
             }
         }
 
+        context?.let { startDiscovery(it) }
+
         return binding.root
     }
 
@@ -46,4 +64,154 @@ class JoinFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        context?.let { startDiscovery(it) }
+    }
+
+    private fun startDiscovery(context: Context) {
+        val discoveryOptions = DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build()
+        Nearby.getConnectionsClient(context)
+            .startDiscovery("com.example.and_projet", endpointDiscoveryCallback, discoveryOptions)
+            .addOnSuccessListener { unused: Void? -> }
+            .addOnFailureListener { e: Exception? -> }
+    }
+
+    private val endpointDiscoveryCallback: EndpointDiscoveryCallback =
+        object : EndpointDiscoveryCallback() {
+            override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
+                // An endpoint was found. We request a connection to it.
+                Log.i("DEBUG", "ENDPOINT FOUND $endpointId, ${info.endpointName}") // TODO
+                Nearby.getConnectionsClient(context!!)
+                    .requestConnection("HOST", endpointId, connectionLifecycleCallback)
+                    .addOnSuccessListener(
+                        OnSuccessListener { unused: Void? ->
+                            // We successfully requested a connection. Now both sides
+                            // must accept before the connection is established.
+                        })
+                    .addOnFailureListener(
+                        OnFailureListener { e: java.lang.Exception? ->
+                            // Nearby Connections failed to request the connection.
+                        })
+            }
+
+            override fun onEndpointLost(endpointId: String) {
+                // A previously discovered endpoint has gone away.
+            }
+        }
+
+    private val connectionLifecycleCallback: ConnectionLifecycleCallback =
+        object : ConnectionLifecycleCallback() {
+            override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
+                AlertDialog.Builder(context)
+                    .setTitle("Accept connection to " + info.endpointName)
+                    .setMessage("Confirm the code matches on both devices: " + info.authenticationDigits)
+                    .setPositiveButton(
+                        "Accept"
+                    ) { dialog: DialogInterface?, which: Int ->  // The user confirmed, so we can accept the connection.
+                        Nearby.getConnectionsClient(context!!)
+                            .acceptConnection(endpointId, payloadCallback)
+                    }
+                    .setNegativeButton(
+                        R.string.cancel
+                    ) { dialog: DialogInterface?, which: Int ->  // The user canceled, so we should reject the connection.
+                        Nearby.getConnectionsClient(context!!).rejectConnection(endpointId)
+                    }
+                    .setIcon(R.drawable.ic_dialog_alert)
+                    .show()
+            }
+
+            override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+                when (result.status.statusCode) {
+                    ConnectionsStatusCodes.STATUS_OK -> {
+                        // We're connected! Can now start sending and receiving data.
+                        Log.i("DEBUG", "ConnectionsStatusCodes.STATUS_OK")
+                    }
+                    ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
+                        // The connection was rejected by one or both sides.
+                        Log.i("DEBUG", "ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED")
+                    }
+                    ConnectionsStatusCodes.STATUS_ERROR -> {
+                        // The connection broke before it was able to be accepted.
+                        Log.i("DEBUG", "ConnectionsStatusCodes.STATUS_ERROR")
+                    }
+                    else -> {
+                        // Unknown status code
+                        Log.i("DEBUG", "Unknown status code")
+                    }
+                }
+            }
+
+            override fun onDisconnected(endpointId: String) {
+                // We've been disconnected from this endpoint. No more data can be
+                // sent or received.
+                Log.i("DEBUG", endpointId)
+            }
+        }
+
+    private val payloadCallback: PayloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            Log.i("DEBUG", String(payload.asBytes()!!, Charsets.UTF_8))
+            println("DEBUG" + String(payload.asBytes()!!, Charsets.UTF_8))
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+            // after the call to onPayloadReceived().
+            Log.i("DEBUG", endpointId)
+        }
+    }
+
+    // PERMISSIONS
+    private val REQUIRED_PERMISSIONS: Array<String> = arrayOf<String>(
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.BLUETOOTH_ADMIN,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.CHANGE_WIFI_STATE,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    private val REQUEST_CODE_REQUIRED_PERMISSIONS = 1
+
+    override fun onStart() {
+        super.onStart()
+        if (!hasPermissions(requireContext(), *REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+        }
+    }
+
+    /** Returns true if the app was granted all the permissions. Otherwise, returns false.  */
+    private fun hasPermissions(context: Context, vararg permissions: String): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    /** Handles user acceptance (or denial) of our permission request.  */
+    @Deprecated("Deprecated in Java")
+    @CallSuper
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return
+        }
+        for (grantResult in grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(requireContext(), "MISSING PERMISSION", Toast.LENGTH_LONG).show()
+//                finish()
+                return
+            }
+        }
+        activity?.recreate()
+    }
+
 }
